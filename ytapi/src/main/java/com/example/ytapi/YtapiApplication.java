e4bdeaa16886097980931491e5e51dd397942fb6
Kushal Sharma <kushalsharma1218@gmail.com>
2023-01-31 20:48:14 +0000

    Adding Spring Boot dependencies --Initial Push

---
+++ ytapi/src/main/java/com/example/ytapi/YtapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ytapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtapiApplication.class, args);
+	}
+
+}
---
+++ ytapi/src/test/java/com/example/ytapi/YtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ytapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
